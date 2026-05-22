0f580a96e3298e2b7357d88a4c66e4646b203d7a
Anuja khatri <khatrianuja20@gmail.com>
2026-05-23 02:05:08 +0545

    remove Bug_report as it has been shifted to BugReport.git

--- Bug_report/Bug_report/bug/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import BugReportAPI
-
-urlpatterns = [
-    path('bug-reports/', BugReportAPI.as_view()),
-]
--- Bug_report/bug_logging.py
+++
@@ -1,3 +0,0 @@
-import logging
-
-logger = logging.getLogger('bug_api')
